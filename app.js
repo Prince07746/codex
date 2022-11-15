b1f6b7de647b0d5e9d7c01fb34a8d04d32f44797
Prince07746 <98363125+Prince07746@users.noreply.github.com>
2022-11-15 14:59:56 +0300

    adding some word to test the app

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/',function(req,res){
-    res.send("welcome to codex");
+    res.send("welcome to codex this prince");
 });
 
 app.listen(port,function(){
