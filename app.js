9c1c83b103cc917e1481bb80b34b5a62b98b4fad
Prince07746 <98363125+Prince07746@users.noreply.github.com>
2022-11-15 15:29:14 +0300

    changed

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var bodyParser = require('body-parser');
 var app = express();
 
 var port = process.env.PORT || 3000;
-app.use(express.static(_dirname));
+app.use(express.static(__dirname));
 app.get('/',function(req,res){
-    res.sendFile(_dirname+"/apk.html");
+    res.sendFile(__dirname+"/apk.html");
 });
 
 app.listen(port,function(){
