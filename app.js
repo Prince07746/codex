1fb5ab3ebd6c1bc9396a71c589a86eab8252ab35
Prince07746 <98363125+Prince07746@users.noreply.github.com>
2023-03-30 11:19:04 +0300

    fixing

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
 var app = express();
 
 var port = process.env.PORT || 3000;
 app.use(express.static(__dirname));
+
 app.get('/',function(req,res){
     console.log(req.ip);
     res.sendFile(__dirname+"/apk.html");
 });
 
-app.listen(port,function(){
+app.listen(port,function(err){
+    if(err) console.log('something is wrong');
     console.log("server started at "+port);
 });
